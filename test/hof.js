ca485469589dd9dde467c9088541a5de061a8993
007Pradip <pradip.inc.bca.2021@gmail.com>
2024-12-26 11:17:22 +0545

    Updated

---
+++ test/hof.js
@@ -0,0 +1,23 @@
+// hof(higher order function)
+// inbuild
+// custom
+
+// function hof(a,b,cd){
+//     return cb(a,b);
+// }
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(hof(1,2,add));
+// function hof(a,b,cb){
+//     return cb(a,b);
+// }
+
+// function mul(a,b){
+//     return a*b;
+// }   
+
+// console.log(hof(1,2,mul));
+
