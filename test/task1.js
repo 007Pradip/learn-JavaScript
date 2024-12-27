065ff01d25d67158946cd81e24e4ee72fde26239
007Pradip <pradip.inc.bca.2021@gmail.com>
2024-12-27 11:18:54 +0545

    Task

--- test/task1.js
+++ test/task1.js
@@ -8,10 +8,15 @@
 //     this.price = price;
 // }
 
+
+
+
+/*Write a function that takes an object representing a book (with properties title, author, year, and genre) and returns a string formatted as:
+"The book <title> by <author> was published in <year>. It falls under <genre>." */
 //creating function of book details
 function booksdetails(book){
-   const {title, author, price, genre} = book;
-   return `The book name is ${title} written by author ${author} and genre is ${genre} and price is Rs. ${price}`;
+   const {title, author, price, genre, year} = book;
+   return `The book name is ${title} written by author ${author} and genre is ${genre} and price is Rs. ${price}. It was published in ${year}.`;
 }
 
 //creating object   
@@ -20,13 +25,20 @@ let book1 = {
     author: 'Paulo Coelho',
     genre: 'Fiction',
     price: 400, 
+    year: 1988,
     book2:{
         title: 'The Secret',
         author: 'Rhonda Byrne',
         genre: 'Self-help',
         price: 500,
+        year: 2006,
     }
 };
 
 console.log(booksdetails(book1));
 console.log(booksdetails(book1.book2));
+
+
+
+
+
---
+++ test/task2.js
@@ -0,0 +1,33 @@
+/*An arrow function expression is a compact alternative to a traditional function expression, with some semantic differences and deliberate limitations in usage: */
+
+//Syntax
+/*
+        () => expression
+
+        param => expression
+
+        (param) => expression
+
+        (param1, paramN) => expression
+
+        () => {
+        statements
+        }
+
+        param => {
+        statements
+        }
+*/
+
+
+/*Write a function to filter out all the numbers greater than 10 from an array using an arrow function. */
+//creating function to filter out numbers greater than 10
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let filterNumbers = arr1.filter(num => num > 10);
+
+console.log(filterNumbers);
+
+//const filterNumbers = (arr) => arr.filter(num => num < 10);
+//console.log(filterNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]));
+
+
